bec38befbbca294ef2adf758c9eef4167db8bb25
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2022-12-09 17:08:57 +0700

    Create about.php

---
+++ view/layout/pages/about.php
@@ -0,0 +1,14 @@
+<?php echo $this->extend('layout/template'); ?>
+
+<?php echo $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aperiam tempora possimus explicabo autem unde nulla enim, corporis impedit libero neque iste quibusdam, iusto esse nisi, repellat dicta blanditiis. Error.</p>
+        </div>
+    </div>
+</div>
+
+<?php echo $this->endSection(); ?>
